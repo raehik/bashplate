35243068d26ffa98a10c145ccf238c9dec9fed12
Ben Orchard <thefirstmuffinman@gmail.com>
2016-03-18 11:07:30 +0000

    initial commit: add current framework files

---
+++ bashplate.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# bashplate: a clean, reusable Bash framework for simple scripts.
+#
+
+# make sure they gave us a directory for bashplate files
+if [[ ! -d "$_BASHPLATE_DIR" ]]; then
+    >&2 echo "\
+bashplate error: \$_BASHPLATE_DIR is \ invalid or undefined: '$_BASHPLATE_DIR'"
+    exit 1
+fi
+
+_FILENAME="${0##*/}"
+
+## Logging {{{
+_LOGGER="echo"
+_LOG_PRE="$_FILENAME"
+
+# If verbose, log a message.
+_log() {
+    # if not verbose (i.e. $VERBOSE is empty), do nothing
+    [[ -z $_VERBOSE ]] && return
+    $_LOGGER "$_LOG_PRE: $1"
+}
+
+# Log an error, regardless of verbosity. If given a 2nd argument, exit with it
+# as the exit code.
+_err() {
+    $_LOGGER "$_LOG_PRE: error: $1" >&2
+    [[ $2 ]] && exit $2
+}
+## }}}
+
+## Usage {{{
+# Print usage then exit depending on given exit code.
+_print_usage() {
+    local pipe=1              # pipe to stdout, or...
+    [[ $1 == 0 ]] && pipe=2   # ...pipe to stderr if argument is non-zero
+    if [[ ! "$_usage" ]]; then
+        _err "no \$_usage string defined" 1
+    fi
+    >&$pipe echo "$_usage"
+   exit $1
+}
+## }}}
+
+## Options {{{
+source "$_BASHPLATE_DIR/getopts_long.sh"
+## }}}
